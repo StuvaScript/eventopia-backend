16c4af7793a0f570af3f47ba35e7a121e6da3835
AmandaHockmuth <153574152+AmandaHockmuth@users.noreply.github.com>
2024-11-18 19:15:04 -0500

    Created model folder and added Itinerary Item Schema, installed joi to handle "required" and "date."

---
+++ models/ItineraryItem.js
@@ -0,0 +1,53 @@
+const { required, date } = require("joi");
+const mongoose = require("mongoose");
+
+const ItineraryItemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Error: no name provided for itinerary item."],
+      maxlength: 25,
+    },
+    date: {
+      type: Date,
+      required: [true, "Error: no date provided for itinerary item."],
+    },
+    location: {
+      address: {
+        type: String,
+        required: [true, "Error: no address provided for itinerary item."],
+      },
+      city: {
+        type: String,
+        required: [true, "Error: no city provided for itinerary item."],
+      },
+      state: {
+        type: String,
+        required: [true, "Error: no state provided for itinerary item."],
+      },
+      coordinates: {
+        lat: {
+          type: Number,
+          required: [true, "Error: no latitude provided for location."],
+        },
+        lng: {
+          type: Number,
+          required: [true, "Error: no longitude provided for location."],
+        },
+      },
+    },
+    status: {
+      type: String,
+      enum: ["interested", "RSVPd"],
+      default: "interested",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user."],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ItineraryItem", ItineraryItemSchema);
