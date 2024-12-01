18a42b06a079d207917e7088f09251382c5f3dee
Sisi Wang <sisiwang242@gmail.com>
2024-12-01 16:45:15 -0500

    add lowercase: true, unique: true to emails, add timestamps: true to user schema. create user.js inside of controllers to handle register and login, create errors and middleware to handle errors and bad requests, add user.js inside of routes to handle register and login, and update app.js with userRouter.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ const app = express();
 const cors = require('cors')
 const favicon = require('express-favicon');
 const logger = require('morgan');
+const authenticateUser = require('./middleware/authentication');
+const notFoundMiddleware = require('./middleware/not_found');
+const errorHandleMiddleware = require('./middleware/error_handler');
+
 
 const mainRouter = require('./routes/mainRouter.js');
+const userRouter = require('./routes/user.js');
 
 // middleware
 app.use(cors());
@@ -14,7 +19,13 @@ app.use(logger('dev'));
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
+
 // routes
 app.use('/api/v1', mainRouter);
+app.use('/api/v1/user', userRouter);
+
+// error handling middleware
+app.use(notFoundMiddleware);
+app.use(errorHandleMiddleware);
 
 module.exports = app;
--- src/models/user.js
+++ src/models/user.js
@@ -22,6 +22,8 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Please provide email'],
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'],
         index: true,
+        lowercase: true,
+        unique: true, 
     },
     password: {
         type: String,
@@ -37,6 +39,9 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function(next) {
+    if (this.email) {
+        this.email = this.email.toLowerCase().trim();
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
