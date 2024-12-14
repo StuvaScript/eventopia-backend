d323691680d0108261c7a4180268f9bcb73850d5
AmandaHockmuth <153574152+AmandaHockmuth@users.noreply.github.com>
2024-12-13 21:52:12 -0500

    installed helmet, express-rate-limit, and express-mongo-sanitize for BE security protection against XSS brute force and injection attacks.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const favicon = require('express-favicon');
-const logger = require('morgan');
+const cors = require("cors");
+const favicon = require("express-favicon");
+const logger = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 
-const mainRouter = require('./routes/mainRouter.js');
-const ticketmasterRouter = require('./routes/ticketmasterRouter.js');
+const mainRouter = require("./routes/mainRouter.js");
+const ticketmasterRouter = require("./routes/ticketmasterRouter.js");
 
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.static('public'))
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(helmet());
+app.use(mongoSanitize());
 
-// routes
-app.use('/api/v1', mainRouter);
-app.use('/api/v1/ticketmaster', ticketmasterRouter);
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 200, // Limit each IP to 200 requests per windowMs
+});
+
+app.use("/api", apiLimiter);
+app.use("/api/v1", mainRouter);
+app.use("/api/v1/ticketmaster", ticketmasterRouter);
 
 module.exports = app;
