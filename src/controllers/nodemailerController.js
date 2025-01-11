79aa68a999e6c9c3a861860a4959d44e9fd125c4
AmandaHockmuth <153574152+AmandaHockmuth@users.noreply.github.com>
2025-01-11 17:29:02 -0500

    set up nodemailer router and finished controller

--- src/controllers/nodemailerController.js
+++ src/controllers/nodemailerController.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+const { StatusCodes } = require("http-status-codes");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com", // provider's SMTP server
@@ -10,18 +11,37 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAILPASSWORD,
   },
 });
+const shareEvent = async (req, res) => {
+  const { recipientEmail, eventDetails, userName } = req.body;
 
-const shareEventEmail = {
-  from: "HHTeamOne@gmail.com",
-  to: recipientEmail,
-  subject: "Check Out this Event",
-  html: "<h1>Hey there!</h1><p>Take a look at this event I found on .....</p>",
+  const shareEventEmail = {
+    from: "HHTeamOne@gmail.com",
+    to: recipientEmail,
+    subject: "Check Out this Event",
+    html: `
+          <h1>Hey there!</h1>
+          <p>${userName} wanted to share this event with you:</p>
+          <h2>${eventDetails.name}</h2>
+          <p>Date: ${eventDetails.date}</p>
+          <p>Location: ${eventDetails.location.address}, ${
+            eventDetails.location.city
+            }, ${eventDetails.location.state}</p>
+          <p>Details: ${eventDetails.info || ""}</p>
+        `,
+  };
+
+  try {
+    const info = await transporter.sendMail(shareEventEmail);
+    res.status(StatusCodes.OK).json({
+      message: "Email sent successfully",
+      info: info.response,
+    });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: "Error sending email",
+      error: error.message,
+    });
+  }
 };
 
-transporter.sendMail(shareEventEmail, (error, info) => {
-  if (error) {
-    console.error("Error sending email:", error);
-  } else {
-    console.log("Email sent:", info.response);
-  }
-});
+module.exports = { shareEvent };
