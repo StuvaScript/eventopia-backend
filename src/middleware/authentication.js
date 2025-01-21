918edce265e2c59a8d5beaaaba75001960cb8bde
Sisi Wang <sisiwang242@gmail.com>
2025-01-20 20:28:14 -0500

    remove csrf

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,17 +1,47 @@
+// require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+// const UnauthenticatedError = require("../errors/unauthenticated");
+
+// const auth = async (req, res, next) => {
+//   const authHeader = req.cookies.token;
+//   console.log("authHeader", authHeader);
+
+//   if (!authHeader) {
+//     throw new UnauthenticatedError("Authentication invalid");
+//   }
+
+//   try {
+//     const payload = jwt.verify(authHeader, process.env.JWT_SECRET);
+//     console.log("JWT Payload:", payload);
+//     req.user = {
+//       userId: payload.userId,
+//       name: `${payload.firstName} ${payload.lastName}`,
+//     };
+//     console.log(`middleware check user`, req.user);
+//     next();
+//   } catch (error) {
+//     throw new UnauthenticatedError("Authentication invalid");
+//   }
+// };
+
+
+// module.exports = auth;
+
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const UnauthenticatedError = require("../errors/unauthenticated");
 
 const auth = async (req, res, next) => {
-  const authHeader = req.cookies.token;
-  console.log("authHeader", authHeader);
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
 
-  if (!authHeader) {
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
     throw new UnauthenticatedError("Authentication invalid");
   }
+  const token = authHeader.split(" ")[1];
 
   try {
-    const payload = jwt.verify(authHeader, process.env.JWT_SECRET);
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
     console.log("JWT Payload:", payload);
     req.user = {
       userId: payload.userId,
@@ -23,6 +53,4 @@ const auth = async (req, res, next) => {
     throw new UnauthenticatedError("Authentication invalid");
   }
 };
-
-
-module.exports = auth;
+module.exports = auth
